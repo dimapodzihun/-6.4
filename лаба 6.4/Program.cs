36f001a1b7f72fef602633b6f31bbef74fd5c319
HP <HP@DESKTOP-7PUF2T6>
2024-11-17 22:27:32 +0200

    Add project files.

---
+++ лаба 6.4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лаба_6._4
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть перший член прогресії (a1):");
+            int a1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введіть різницю прогресії (d):");
+            int d = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введіть кількість членів прогресії (n):");
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = ArithmeticSum(a1, d, n);
+            Console.WriteLine($"Сума перших {n} членів арифметичної прогресії: {sum}");
+        }
+        static int ArithmeticSum(int a1, int d, int n)
+        {
+            if (n == 1)
+                return a1;
+            else
+            {
+            return a1 + ArithmeticSum(a1 + d, d, n - 1); 
+            }
+        }
+    }
+}
